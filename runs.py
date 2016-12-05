a1a33c5f5c62e1a9c3b0b06bac9909ecc9fc8384
Ilone <ilonemail2@gmail.com>
2016-12-05 17:16:48 +0100

    Stop and resume corrected and functionnal

--- runs.py
+++ runs.py
@@ -12,41 +12,36 @@ nb_test = 10
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 
-runsqueue = [''] * (comm.Get_size()-1)#because first is master and does not work
-lrnf = ''#last run not finished
-follow_previous = isfile('current_run.dat')
+runsqueue = [(len(ks), len(dims), len(funcs), nb_test)] * (comm.Get_size()-1)#because first is master and does not work
+
+if isfile('current_run.dat'):
+    lrnf = eval(open('current_run.dat', 'r').read())#last run not finished
 
 if rank == 0:
     for k in range(len(ks)):
         for d in range(len(dims)):
             for f in range(len(funcs)):
                 for test in range(nb_test):
-                    if follow_previous:
-                        previous_data = eval(open('current_run.dat', 'r').read())
-                        k = previous_data['ki']
-                        d = previous_data['di']
-                        f = previous_data['fi']
-                        test = previous_data['ti']
-                        follow_previous = False
-
+                    if((k, d, f, test) < lrnf):
+                        continue#because changing vars directly has no effect on for loops; Note: this is a tricky and ugly workaround!!
                     runner = comm.recv(tag=0)
                     run_data = {'ki': k, 'di': d, 'fi': f, 'ti': test}
                     comm.send(run_data, dest=runner, tag=1)
                     print("running test %d/%d for k=%d d=%d f=%s" % (test+1, nb_test, ks[k], dims[d], funcs[f].__name__))
 
-                    runsqueue[runner-1] = str(run_data)
+                    runsqueue[runner-1] = (k, d, f, test)
                     if lrnf < min(runsqueue):
                         lrnf = min(runsqueue)
-                        open('current_run.dat', 'w').write(lrnf)
+                        open('current_run.dat', 'w').write(str(lrnf))
 
     for i in range(comm.Get_size()-1):
         runner = comm.recv(tag=0)
         comm.send(False, dest=runner, tag=1)
 
-        runsqueue[runner-1] = '{}'
+        runsqueue[runner-1] = (len(ks), len(dims), len(funcs), nb_test)
         if lrnf < min(runsqueue):
             lrnf = min(runsqueue)
-            open('current_run.dat', 'w').write(lrnf)
+            open('current_run.dat', 'w').write(str(lrnf))
 
 #if rank == 1:
 #TODO: compute stats
