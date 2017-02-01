5b6ff0db8dc7d400ddaddf4510f4ddc3adfcf3e8
Trop-TTA <trottalukas@gmail.com>
2017-02-01 10:51:15 +0100

    adding special func

--- sumafcts.py
+++ sumafcts.py
@@ -1,4 +1,7 @@
 import numpy as np
+from owncma import Rotation
+
+rotate = Rotation()
 
 def translated_sphere(X, args=()):
     c = [1] * len(X);
@@ -6,4 +9,12 @@ def translated_sphere(X, args=()):
         c = args;
     trX = X - c;
     return np.dot(trX,trX);
+
+def weightednorm(X, args=()):
+    D = len(X)
+    Z = rotate(X)
+    res = 0
+    for i in range(D):
+        res += 10 ** (-i) * Z[i] ** 2
+    return res
 
